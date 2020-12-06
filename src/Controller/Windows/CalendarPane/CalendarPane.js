68c8e9badc109778da20e2433b0cfcc711781e95
QoobSweet <qoobsweet@gmail.com>
2020-12-06 11:16:31 -0500

    Calendar Update
    - clicking event in Month/Day view will now open client/inquiry and scroll timeline view to event
    - clicking calendar day in Month/Day view will now scroll Timeline View to selected day

--- src/Controller/Windows/CalendarPane/CalendarPane.js
+++ src/Controller/Windows/CalendarPane/CalendarPane.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import qds_Custom, { Definitions } from '../../resources/qds_Library/qds_custom.js';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
@@ -8,142 +8,126 @@ import resourceTimelinePlugin from '@fullcalendar/resource-timeline';
 import resourceAreaColumns from './resources/resourceAreaColumns.js';
 import './CalendarPane.css';
 
-const CalendarPane = (props) => {    
-    let events = null;
+const CalendarPane = (props) => {
+    const [calendar, setCalendar] = useState(null);
+    const [timeline, setTimeline] = useState(null);
+    const [scrollToTime, setScrollToTime] = useState(null);
+    const [calendarRef, ] = useState(React.createRef());
+    const [timelineRef, ] = useState(React.createRef());
 
-    //#region functions
-    const [selectedDate, setSelectedDate] = useState(Date.now.to)
 
-    const generateCalendarEvents = (inquiries) => {
-        const events = [];
-
-        if(inquiries){
-            for(let i = 0; i < inquiries.length; i++){
-                const target = inquiries[i];
-                if(target){
-                    const startDate = target.eventDate + 'T' + target.startTime + ':00';
-                    const stopDate = target.eventDate + 'T' + target.stopTime + ':00';
-
-                    events.push(new Definitions.Event(target.id, target.eventTitle, startDate, stopDate, inquiries[i].eventStatus))
+    useEffect(() => {
+        const handleDateSelect = (selectInfo) => {
+            console.log(selectInfo);
+            
+            timelineRef.current._calendarApi.gotoDate(selectInfo.startStr)
+            //calendar.props.children.setScrollToTime(dateObj)
+    
+            //prompt(selectInfo.startStr)
+            let calendarApi = selectInfo.view.calendar
+    
+            calendarApi.unselect() // clear date selection
+        }
+        const handleEventChange = (apiResponse) => {
+            if(apiResponse.oldEvent && apiResponse.event){
+                const eventDbId = apiResponse.event.id;
+    
+                const oldEvent = apiResponse.oldEvent;
+                const newEvent = apiResponse.event;
+                
+                const oldStartDate = oldEvent.startStr.split('T');
+                const newStartDate = newEvent.startStr.split('T');
+    
+                const oldEndDate = oldEvent.endStr.split('T');
+                const newEndDate = newEvent.endStr.split('T');
+    
+                //compare dates [0]
+                if(newStartDate[0] !== oldStartDate[0]){
+                    qds_Custom.dbSetValue('inquiries', eventDbId, 'eventDate', newStartDate[0]);
+                }
+    
+                //compare times [1]
+                if(newStartDate[1] !== oldStartDate[1]){
+                    qds_Custom.dbSetValue('inquiries', eventDbId, 'startTime', newStartDate[0]);
+                }
+                if(newEndDate[1] !== oldEndDate[1]){
+                    qds_Custom.dbSetValue('inquiries', eventDbId, 'stopTime', newStartDate[0]);
                 }
             }
         }
-
-        if(events[0]){
-            return events;
-        } else {
-            return false;
+        const generateCalendarEvents = (inquiries) => {
+            const events = [];
+    
+            if(inquiries){
+                for(let i = 0; i < inquiries.length; i++){
+                    const target = inquiries[i];
+                    if(target){
+                        const startDate = target.eventDate + 'T' + target.startTime + ':00';
+                        const stopDate = target.eventDate + 'T' + target.stopTime + ':00';
+    
+                        events.push(new Definitions.Event(target.id, target.eventTitle, startDate, stopDate, inquiries[i].eventStatus))
+                    }
+                }
+            }
+    
+            if(events[0]){
+                return events;
+            } else {
+                return false;
+            }
         }
-    }
+        const selectEvent = (eventInfo) => {
+            const calendarApi = timelineRef.current._calendarApi
+            console.log(calendarApi)
+            calendarApi.gotoDate(eventInfo.event.startStr);
+            calendarApi.scrollToTime(eventInfo.event.startStr.split('T')[1])
 
-    const handleEventChange = (apiResponse) => {
-        if(apiResponse.oldEvent && apiResponse.event){
-            const eventDbId = apiResponse.event.id;
-
-            const oldEvent = apiResponse.oldEvent;
-            const newEvent = apiResponse.event;
+            props.setSelectedInquiry(eventInfo.event.id);
             
-            const oldStartDate = oldEvent.startStr.split('T');
-            const newStartDate = newEvent.startStr.split('T');
-
-            const oldEndDate = oldEvent.endStr.split('T');
-            const newEndDate = newEvent.endStr.split('T');
-
-            //compare dates [0]
-            if(newStartDate[0] !== oldStartDate[0]){
-                qds_Custom.dbSetValue('inquiries', eventDbId, 'eventDate', newStartDate[0]);
-            }
-
-            //compare times [1]
-            if(newStartDate[1] !== oldStartDate[1]){
-                qds_Custom.dbSetValue('inquiries', eventDbId, 'startTime', newStartDate[0]);
-            }
-            if(newEndDate[1] !== oldEndDate[1]){
-                qds_Custom.dbSetValue('inquiries', eventDbId, 'stopTime', newStartDate[0]);
-            }
         }
-    }
-
-    const handleDateSelect = (selectInfo) => {
-        console.log(selectInfo);
-        const selectedDate = selectInfo.startStr;
-        console.log(selectedDate);
-        const subDateArray = selectedDate.split('-')
-        console.log(subDateArray);
-        const dateObj = {
-            year: subDateArray[0],
-            month: subDateArray[1],
-            dat: subDateArray[2]
-        }
-        console.log(dateObj);
+        const  renderEventContent = (eventInfo) => {
+            function addZero(i) {
+                if (i < 10) {
+                    i = "0" + i;
+                } else if (i > 12) {
+                    i = i - 12;
+                }
+                return i;
+              }
+            
+            
+            const start = eventInfo.event.start; //date object
+            const end = eventInfo.event.end; //date object
     
-
-        //prompt(selectInfo.startStr)
-        let calendarApi = selectInfo.view.calendar
-
-        calendarApi.unselect() // clear date selection
-    }
-
-    const handleEvent = (event) => {
-        console.log(event)
-    }
-
-    const handleEvents = (events) => {
-        // called after events are initialized/added/changed/removed   
-
-    }
-
-    const selectEvent = (eventInfo) => {
-        props.setSelectedInquiry(eventInfo.event.id)
-    }
-
-    const  renderEventContent = (eventInfo) => {
-        function addZero(i) {
-            if (i < 10) {
-                i = "0" + i;
-            } else if (i > 12) {
-                i = i - 12;
+            const sHour = addZero(start.getHours());
+            const sMin = addZero(start.getMinutes());
+            const sString = sHour + ':' + sMin
+    
+            let eString = '';
+    
+            if(end){
+                const eHour = addZero(end.getHours());
+                const eMin  = addZero(end.getMinutes());
+                eString = '-' + eHour + ':' + eMin;
             }
-            return i;
-          }
-        
-        
-        const start = eventInfo.event.start; //date object
-        const end = eventInfo.event.end; //date object
-
-        const sHour = addZero(start.getHours());
-        const sMin = addZero(start.getMinutes());
-        const sString = sHour + ':' + sMin
-
-        let eString = '';
-
-        if(end){
-            const eHour = addZero(end.getHours());
-            const eMin  = addZero(end.getMinutes());
-            eString = '-' + eHour + ':' + eMin;
+    
+            return (
+                <>
+                    {/* <b className="cal-popup-disp-time">{sString +  eString}</b> */}
+                    <i className="cal-popup-disp-item">{eventInfo.event.title}</i>
+                </>
+            )
         }
 
-        return (
-            <>
-                <b className="cal-popup-disp-time">{sString +  eString}</b>
-                <i className="cal-popup-disp-item">{eventInfo.event.title}</i>
-            </>
-        )
-    }
-
-    //#endregion
-
-    events = generateCalendarEvents(props.inquiries);
-    
-    const getCalendar = () => {
-        return (
-            <div className="-AppContent">
+        const events = generateCalendarEvents(props.inquiries);
+        const getCalendar = () => {
+            return (
                 <FullCalendar
                     plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
                     headerToolbar={{
-                        left: 'prev,next today',
-                        center: 'title',
-                        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                        left: 'title',
+                        center: '',
+                        right: 'today prev next'
                     }}
                     initialView='dayGridMonth'
                     editable={true}
@@ -156,57 +140,69 @@ const CalendarPane = (props) => {
                     eventClick={selectEvent}
                     eventChange={handleEventChange}
                     eventContent={renderEventContent} // custom render function
-                    eventLimit='0'
-
-                    eventsSet={handleEvents} // called after events are initialized/added/changed/removed
-                    
+                    eventLimit='3'
+                    ref={calendarRef}
                     /* you can update a remote database when these fire:
                     eventAdd={function(){}}
                     eventChange={function(){}}
                     eventRemove={function(){}}
                     */
                 />
-            </div>
-        )
-    }
-    const getTimeline = () => {
-        return (
-            <div className="-AppContent">
-                <FullCalendar 
-                    plugins={[resourceTimelinePlugin]}
-                    schedulerLicenseKey='CC-Attribution-NonCommercial-NoDerivatives'
-                    timeZone='UTC'
-                    initialView='resourceTimelineDay'
-                    aspectRatio= {1.7}
-                    slotMinWidth= {25}
-                    scrollTime='8:00:00'
-                    headerToolbar= {{
-                        left: 'prev,next',
-                        center: 'title',
-                        right: 'resourceTimelineDay'
-                    }}
-                    editable={true}
-                    eventStartEditable={true}
-                    selectable= {true}
-                    resourceAreaColumns={resourceAreaColumns}
-                    resourceOrder= 'tOrder'
-                    resources={Definitions.Event.resources()}
-                    events={events}
-                    eventClick={selectEvent}
-                />
-            </div>
-        )
-    }
+            )
+        }
+        const getTimeline = () =>{ 
+            return (
+                    <FullCalendar 
+                        plugins={[resourceTimelinePlugin]}
+                        schedulerLicenseKey='CC-Attribution-NonCommercial-NoDerivatives'
+                        timeZone='UTC'
+                        initialView='resourceTimelineDay'
+                        aspectRatio= {1.7}
+                        slotMinWidth= {25}
+                        scrollTime='9:00:00'
+                        headerToolbar= {{
+                            left: 'title',
+                            center: '',
+                            right: 'prev next'
+                        }}
+                        editable={true}
+                        eventStartEditable={true}
+                        selectable= {true}
+                        resourceAreaColumns={resourceAreaColumns}
+                        resourceOrder= 'tOrder'
+                        resources={Definitions.Event.resources()}
+                        events={events}
+                        eventClick={selectEvent}
+                        ref={timelineRef}
+                    />
+            )
+        }
+        
+        setCalendar(getCalendar())
+        setTimeline(getTimeline())
+
+    }, [props, scrollToTime])
+
+
+    //#endregion
 
 
     return (
-        <div className='CalendarPane-basicCalendar-container'>
-            {getCalendar()}
+        <>
+            <div className='CalendarPane-basicCalendar-container'>
+                <div className="-AppContent">
+                    {calendar}
+                </div>
+            </div>
 
             {qds_Custom.Divider()}
 
-            {getTimeline()}
-        </div>
+            <div className='CalendarPane-basicCalendar-container'>
+                <div className="-AppContent">
+                    {timeline}
+                </div>
+            </div>
+        </>
     )
 }
     
